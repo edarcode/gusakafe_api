f7671545ea19ecaa789f2695a3cd345740e1c272
Edwin Ortiz <edarcode@gmail.com>
2022-06-20 12:17:32 -0500

    feat(src/routes/categories/getController/getAllCategories.controller.js) consultar todas las categorias

--- src/routes/categories/categories.router.js
+++ src/routes/categories/categories.router.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
+const getAllCategory = require("./getController/getAllCategories.controller");
 const createCategory = require("./postController/createCategory.controller");
 const categories = Router();
 
 categories.route("/").post(createCategory);
+categories.route("/").get(getAllCategory);
 
 module.exports = categories;
--- src/routes/categories/getController/getAllCategories.controller.js
+++ src/routes/categories/getController/getAllCategories.controller.js
@@ -1,11 +1,12 @@
-/* const { Category } = require("../../../db");
+const { Category } = require("../../../db");
 
 const getAllCategory = async (req, res, next) => {
 	try {
-
+		const categories = await Category.findAll();
+		res.status(200).json(categories);
 	} catch (error) {
 		next(error);
 	}
 };
 
-module.exports = getAllCategory; */
+module.exports = getAllCategory;
