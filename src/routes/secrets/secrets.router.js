10f240a7275cea15f38f35f3838f7ce1cb2cb1e7
Edwin Ortiz <edarcode@gmail.com>
2022-06-21 13:46:48 -0500

    feat(src/routes/secrets) consultar todos los secrets

--- src/routes/secrets/secrets.router.js
+++ src/routes/secrets/secrets.router.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
+const getAllSecrets = require("./getController/getAllSecrets.controller");
 const createSecret = require("./postController/createSecret.controller");
 const secrets = Router();
 
 secrets.route("/").post(createSecret);
+secrets.route("/").get(getAllSecrets);
 
 module.exports = secrets;
