3a0ec3a296f7f3e478d6ea730807730cae3f7e0f
Edwin Ortiz <edarcode@gmail.com>
2022-06-21 14:02:53 -0500

    feat(src/routes/secrets/deleteController/) eliminar secret

--- src/routes/secrets/secrets.router.js
+++ src/routes/secrets/secrets.router.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
+const deleteSecret = require("./deleteController/deleteSecret.controller");
 const getAllSecrets = require("./getController/getAllSecrets.controller");
 const createSecret = require("./postController/createSecret.controller");
 const secrets = Router();
 
 secrets.route("/").post(createSecret);
 secrets.route("/").get(getAllSecrets);
+secrets.route("/").delete(deleteSecret);
 
 module.exports = secrets;
