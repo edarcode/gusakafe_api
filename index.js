120eed090df0fca806bb0de50ba99cd3e9248451
Edwin Ortiz <edarcode@gmail.com>
2022-06-20 22:40:56 -0500

    feat(src/utils/fillCategory.js) inyectando data en el modelo Category

--- index.js
+++ index.js
@@ -20,11 +20,13 @@
 require("dotenv").config();
 const server = require("./src/server.js");
 const { conn } = require("./src/db.js");
+const fillCategory = require("./src/utils/fillCategory.js");
 const PORT = process.env.PORT;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-	server.listen(PORT, () => {
+	server.listen(PORT, async () => {
+		await fillCategory();
 		console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
 	});
 });
