0e91885fcf68ebfdacfc79da968b3e723078870e
Edwin Ortiz <edarcode@gmail.com>
2022-06-21 23:03:57 -0500

    feat() actualizar chef

--- src/routes/chefs/chefs.router.js
+++ src/routes/chefs/chefs.router.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const getAllChefs = require("./getController/getAllChefs.controller");
 const createChef = require("./postController/createChef.controller");
+const updateChef = require("./putController/updateProduct.controller");
 const chefs = Router();
 
 chefs.route("/").post(createChef);
 chefs.route("/").get(getAllChefs);
+chefs.route("/").put(updateChef);
 
 module.exports = chefs;
