cfe310565ddd8ab8267386eb6d8d9c38cff4be7d
Edwin Ortiz <edarcode@gmail.com>
2022-06-22 00:44:02 -0500

    feat(src/routes/tables/getController/getAllTables.controller.js) consultar mesas con filtro por name y state y entregadas paginadas

--- src/constants/perPage.js
+++ src/constants/perPage.js
@@ -1,4 +1,5 @@
 module.exports = {
 	productsPerPage: 8,
-	chefsPerPage: 8
+	chefsPerPage: 8,
+	tablesPerPage: 8
 };
--- src/routes/tables/getController/getAllTables.controller.js
+++ src/routes/tables/getController/getAllTables.controller.js
@@ -1,8 +1,25 @@
-// const { Table } = require("../../../db");
+const { tablesPerPage } = require("../../../constants/perPage");
+const { Table, Op } = require("../../../db");
+const { where } = require("../utils/where");
 
 const getAllTables = async (req, res, next) => {
+	const { page = 0, name, state } = req.query;
 	try {
-		res.json("tables");
+		const { count, rows } = await Table.findAndCountAll({
+			where: where({ name, Op, state }),
+			offset: tablesPerPage * page,
+			limit: tablesPerPage,
+			attributes: ["id", "name", "code", "state"]
+		});
+		if (!rows.length) return res.status(404).json({ msg: "not found" });
+		const data = {
+			page: parseInt(page),
+			tablesPerPage,
+			pageCount: Math.ceil(count / tablesPerPage),
+			tablesCount: count,
+			tables: rows
+		};
+		res.status(200).json(data);
 	} catch (error) {
 		next(error);
 	}
